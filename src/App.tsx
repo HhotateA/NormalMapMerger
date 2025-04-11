ca3130e7c6ffb34817e175fe043718aeb667ad25
HhotateA <hhotatea@gmail.com>
2025-04-12 04:34:11 +0900

    tag追加

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ function App() {
       <NormalMapMerger/>
       <LicenseNotice />
       <ToolDescription/>
+      <footer>
+        <p><small>Site create by <a href="https://www.hhotatea.com/">ほたてねこまじん</a></small></p>
+        <p><small>サイトの閲覧情報は、サイト管理者に送信される可能性があります。  — プライバシーポリシー <a href="https://www.hhotatea.com/privacypolicy.html">PrivacyPolicy</a></small></p>
+      </footer>
     </div>
   )
 }
