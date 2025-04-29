56efce3efcf8965f6cc162ad4ec1eaf5e4eb3461
HhotateA <hhotatea@gmail.com>
2025-04-29 21:46:03 +0900

    GA4イベント登録

--- src/components/CanvasDisplay.tsx
+++ src/components/CanvasDisplay.tsx
@@ -5,11 +5,23 @@ type Props = {
   downloadUrl: string;
 };
 
+
 /**
  * 合成結果のキャンバス表示とダウンロードリンク
  * 表示サイズは常に固定（512×512）でアスペクト比が崩れないようにする
+ * 同時にGA4のイベントを飛ばす。
  */
 export default function CanvasDisplay({ canvasRef, downloadUrl }: Props) {
+  const handleDownloadClick = () => {
+    if (typeof gtag !== "undefined") {
+      gtag('event', 'download_normal_map', {
+        event_category: 'normal_map_tool',
+        event_label: 'Normal Map Download',
+        value: 1
+      });
+    }
+  };
+
   return (
     <div className="mt-4">
       <div className="canvas-wrapper">
@@ -20,6 +32,7 @@ export default function CanvasDisplay({ canvasRef, downloadUrl }: Props) {
           href={downloadUrl}
           download="merged_normal.png"
           className="download-link"
+          onClick={handleDownloadClick}
         >
           合成画像をダウンロード
         </a>
